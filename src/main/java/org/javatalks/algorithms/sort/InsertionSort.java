3c6ee812ff2223e101b75f79ee222e0803ea011c
ctapobep <ctapobep@javatalks.ru>
2013-01-18 13:37:13 +0400

    insertion sort

---
+++ src/main/java/org/javatalks/algorithms/sort/InsertionSort.java
@@ -0,0 +1,68 @@
+package org.javatalks.algorithms.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This algorithm first goes upwards and checks weather current element is smaller than previous one. If yes, then it
+ * goes downwards and insert current element into the position where {@code previousElement =< currentElement =<
+ * nexElement}. And then we move our marker next and check with previous element again.
+ *
+ * @author stanislav bashkirtsev
+ * @see <a href="http://en.wikipedia.org/wiki/Insertion_sort">Wikipedia</a>
+ * @see <a href="http://www.youtube.com/watch?v=Kg4bqzAqRBM&feature=player_detailpage#t=601s">MIT Lecture</a>
+ * @see <a href="http://www.youtube.com/watch?v=ROalU379l3U">Romanian Folk Dance</a>
+ */
+public class InsertionSort implements Sorter {
+    @Override
+    public <E extends Comparable<? super E>> List<E> sort(List<E> toSort) {
+        if (toSort == null || toSort.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<E> result = new ArrayList<>(toSort);
+        return doSort(result);
+    }
+
+    /**
+     * Sorts the incoming list. The specified list is modified.
+     *
+     * @param toSort a list to be sorted, should be already checked for nulls and emptiness
+     * @param <E>    sortable
+     * @return the same list as was passed, but already after sorting
+     */
+    private <E extends Comparable<? super E>> List<E> doSort(List<E> toSort) {
+        for (int i = 1; i < toSort.size(); i++) {
+            int comparison = toSort.get(i).compareTo(toSort.get(i - 1));
+            if (comparison < 0) {
+                Collections.swap(toSort, i, i - 1);
+                sortDown(toSort, i - 1);
+            }
+        }
+        return toSort;
+    }
+
+    /**
+     * Puts the element at {@code indexToStartFrom} into the position it fits where left element is less or equal to the
+     * {@code startFrom} and right element is bigger.
+     *
+     * @param toSort           list to sort (move {@code indexToStartFrom} element to the right position, the rest is
+     *                         sorted yet)
+     * @param indexToStartFrom an element that might be not in the right position, e.g. if we consider this stage:
+     *                         {@code 1, 2, 1, 3} and {@code indexToStartFrom = 2}, then we should move that element to
+     *                         position 1
+     * @param <E>              the comparable
+     * @return the same list that was passed
+     */
+    private <E extends Comparable<? super E>> List<E> sortDown(List<E> toSort, int indexToStartFrom) {
+        for (int i = indexToStartFrom; i >= 1; i--) {
+            int comparison = toSort.get(i).compareTo(toSort.get(i - 1));
+            if (comparison < 0) {
+                Collections.swap(toSort, i, i - 1);
+            } else {
+                break;
+            }
+        }
+        return toSort;
+    }
+}
---
+++ src/test/java/org/javatalks/algorithms/sort/SorterTest.groovy
@@ -0,0 +1,58 @@
+package org.javatalks.algorithms.sort
+
+import org.junit.Test
+
+/**
+ * @author stanislav bashkirtsev
+ */
+class SorterTest {
+    @Test
+    void "already sorted should return same order"() {
+        List<Integer> result = sut.sort([-1, 0, 1, 2])
+        assert result == [-1, 0, 1, 2]
+    }
+
+    @Test
+    void "opposite order case"() {
+        List<Integer> result = sut.sort([2, 1, 0, -1])
+        assert result == [-1, 0, 1, 2]
+    }
+
+    @Test
+    void "only negative numbers"() {
+        List<Integer> result = sut.sort([-2, -1, -15])
+        assert result == [-15, -2, -1]
+    }
+
+    @Test
+    void "only positives"() {
+        List<Integer> result = sut.sort([2, 1, 15])
+        assert result == [1, 2, 15]
+    }
+
+    @Test
+    void "only zeroz"() {
+        List<Integer> result = sut.sort([0, 0, 0])
+        assert result == [0, 0, 0]
+    }
+
+    @Test
+    void "boundary values"() {
+        List<Integer> result = sut.sort([Integer.MAX_VALUE, 0, Integer.MIN_VALUE])
+        assert result == [Integer.MIN_VALUE, 0, Integer.MAX_VALUE]
+    }
+
+    @Test
+    void "empty returns as empty list"(){
+        List result = sut.sort([])
+        assert result == []
+    }
+
+    @Test
+    void "null returns as empty list"(){
+        List result = sut.sort(null)
+        assert result == []
+    }
+
+    private Sorter sut = new InsertionSort()
+}
