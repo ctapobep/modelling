6ad57bb5a028156b0808369ffae062fe279af6c7
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2021-07-15 09:53:26 +0300

    Check DotProduct linearity in the 1st arg

--- src/test/java/abstractalgebra/column/DotProductTest.java
+++ src/test/java/abstractalgebra/column/DotProductTest.java
@@ -3,7 +3,9 @@ package abstractalgebra.column;
 import abstractalgebra.abstractions.Field;
 import abstractalgebra.abstractions.FieldElement;
 import abstractalgebra.reals.Real;
+import abstractalgebra.reals.RealAddition;
 import abstractalgebra.reals.RealField;
+import abstractalgebra.reals.RealMultiplication;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,7 +15,7 @@ public class DotProductTest {
         Field<Real> realField = RealField.create();
         ColumnGenerator<Real> columnGenerator = new ColumnGenerator<>(realField);
         FieldColumn<Real> v = columnGenerator.random();
-        double result = new DotProduct().calc(v, v).asDouble();
+        double result = innerProduct(v, v).asDouble();
         if(v.equals(columnGenerator.zero()))
             assertEquals(0, result, 0);
         else
@@ -24,13 +26,42 @@ public class DotProductTest {
         Field<Real> realField = RealField.create();
         FieldColumn<Real> v1 = new FieldColumn<>(new FieldElement[]{realField.create(new Real(1)), realField.create(new Real(2))});
         FieldColumn<Real> v2 = new FieldColumn<>(new FieldElement[]{realField.create(new Real(-1)), realField.create(new Real(5))});
-        assertEquals(new Real(9), new DotProduct().calc(v1, v2));
+        assertEquals(new Real(9), innerProduct(v2, v1));
     }
     @Test public void zeroVectorTimesAnythingIsZeroVector() {
         Field<Real> realField = RealField.create();
         ColumnGenerator<Real> columnGenerator = new ColumnGenerator<>(realField);
         FieldColumn<Real> v = columnGenerator.random(),
                           zero = columnGenerator.zero();
-        assertEquals(new Real(0), new DotProduct().calc(zero, v));
+        assertEquals(new Real(0), innerProduct(v, zero));
+    }
+    @Test public void firstArgumentScalingIsLinear() {
+        Field<Real> realField = RealField.create();
+        FieldElement<Real> scalar = realField.random();
+
+        ColumnGenerator<Real> columnGenerator = new ColumnGenerator<>(realField);
+        FieldColumn<Real> v1 = columnGenerator.random(),
+                          v2 = columnGenerator.random(),
+                          v1TimesScalar = new ColumnScalarMultiplication<>(realField, columnGenerator.dims()).calcLeft(scalar, v1);
+
+        assertEquals(
+                new RealMultiplication().calc(innerProduct(v2, v1), scalar.val()),
+                innerProduct(v2, v1TimesScalar));
+    }
+    @Test public void firstArgumentAddingIsLinear() {
+        Field<Real> realField = RealField.create();
+
+        ColumnGenerator<Real> columnGenerator = new ColumnGenerator<>(realField);
+        FieldColumn<Real> v1 = columnGenerator.random(),
+                          v2 = columnGenerator.random(),
+                          v3 = columnGenerator.random(),
+                          v1PlusV2 = new ColumnAddition<>(realField, columnGenerator.dims()).calc(v1, v2);
+        assertEquals(
+                innerProduct(v1PlusV2, v3),
+                new RealAddition().calc(innerProduct(v1, v3), innerProduct(v2, v3)));
+    }
+
+    private Real innerProduct(FieldColumn<Real> v2, FieldColumn<Real> v1TimesScalar) {
+        return new DotProduct().calc(v1TimesScalar, v2);
     }
 }
